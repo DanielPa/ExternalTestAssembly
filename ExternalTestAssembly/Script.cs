460d4c5073baf7fb706178d9b02798c4cff7d3fc
DanielPa <papp.technik@gmail.com>
2015-03-22 00:51:18 +0100

    First upload

---
+++ ExternalTestAssembly/Script.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace ExternalTestAssembly
+{
+    public class Script
+    {
+        [Start]
+        public void Run()
+        {
+            //AbsoluteAssemblyPath: for example "C:\Users\Public\EPLAN\Data\Binaries\MyWPFControlLibrary.dll
+            Assembly myAssembly = Assembly.LoadFrom(@"AbsoluteAssemblyPath");
+
+            //Namespace.ClassName: absolute name of the class to instaciate 
+            //"InstAttibute": if the constructor of the class needs some attributes
+            Object objectOfTestClass = myAssembly.CreateInstance("Namespace.ClassName", false, BindingFlags.ExactBinding, null, new Object[] {"InstAttibute"}, null, null);
+
+            //Namespace.ClassName: again the class name to get the methode to execute
+            //MethodName: Name of the method to execute
+            MethodInfo show = myAssembly.GetType("Namespace.ClassName").GetMethod("MethodName");
+
+            //result: remove if methode has no return type (void)
+            //"MethodAttribute": attributes for the methode (could also be empty)
+            Boolean result = (Boolean)show.Invoke(objectOfTestClass, new Object[]{ "MethodAttribute" });
+
+            //go on with simple scriptcode
+            if (result == true)
+            {
+                MessageBox.Show("Welcome to wonderland!");
+            }
+            else
+            {
+                MessageBox.Show("So you're not the chosen one!");
+            }
+            
+        }
+    }
+}
---
+++ WpfCustomControlLibrary1/Window1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfCustomControlLibrary1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();            
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }        
+    }
+}
